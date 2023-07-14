9d7ce18feb7eb715a8aaf854152d306a46cdf9af
deelaka99 <123wejith@gmail.com>
2023-07-14 11:58:21 +0530

    charts are completed

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,11 +1,71 @@
 import "./chart.scss";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+const data = [
+  {
+    name: "January",
+    Total: 1200,
+  },
+  {
+    name: "February",
+    Total: 2100,
+  },
+  {
+    name: "March",
+    Total: 800,
+  },
+  {
+    name: "April",
+    Total: 1600,
+  },
+  {
+    name: "May",
+    Total: 900,
+  },
+  {
+    name: "June",
+    Total: 1700,
+  },
+];
 
 const Chart = () => {
   return (
     <div className="chart">
-      chart
+      <div className="title">Last 6 Months (Revenue)</div>
+      <ResponsiveContainer width="100%" aspect={2 / 1}>
+        <AreaChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" className="chartsGrid"/>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="Total"
+            stackId="1"
+            stroke="#8884d8"
+            fill="#8884d8"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
     </div>
-  )
-}
+  );
+};
 
-export default Chart
+export default Chart;
