c34812c53fcd6c2dcf3c5ce280671ea572af19ab
deelaka99 <123wejith@gmail.com>
2023-07-15 17:09:22 +0530

    list is completed

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import "./single.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Chart from "../../components/chart/Chart";
+import Table from "../../components/table/Table"
 
 const Single = () => {
   return (
-    <div>
-      Single
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <div className="editButton">Edit</div>
+              <img
+                src="https://images.pexels.com/photos/3250362/pexels-photo-3250362.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+                className="itemImg"
+              />
+              <div className="details">
+                <h1 className="itemTitle">Deelaka Wejith</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">deelaka@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">077 133 9343</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">105/01/136, Horagala East, Padukka.</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">Sri Lanka</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart aspect={3 / 1} title="User Spending ( Last 6 Months )"/>
+          </div>
+        </div>
+        <div className="bottom">
+          <h1 className="title">Last Transactions</h1>
+          <Table />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Single
+export default Single;
